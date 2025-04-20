f23f2e52de885338ec5e409f01181c0ed67e585c
NoteDance <notedance@outlook.com>
2025-04-21 01:03:19 +0800

    Update galore.py

--- galore.py
+++ galore.py
@@ -6,7 +6,6 @@ Copyright 2025 NoteDance
 import tensorflow as tf
 from keras.src.optimizers import optimizer
 import math
-from typing import Literal
 
 
 class GaLore(optimizer.Optimizer):
@@ -55,7 +54,6 @@ class GaLore(optimizer.Optimizer):
         self.projection_type = projection_type
     
     def reset(self):
-        self.step = 0
         iterations = tf.Variable(
                 0,
                 name="iteration",
@@ -72,6 +70,7 @@ class GaLore(optimizer.Optimizer):
             self.exp_avg_sq[self._get_variable_index(var)] =  self.add_variable_from_reference(
                                                         reference_variable=var, name="exp_avg_sq"
                                                     )
+            self.step[self._get_variable_index(var)] = 0
 
     def build(self, var_list):
         if self.built:
@@ -80,7 +79,7 @@ class GaLore(optimizer.Optimizer):
         self.exp_avg = []
         self.exp_avg_sq = []
         self.projector = []
-        self.step = 0
+        self.step = []
         for var in var_list:
             self.exp_avg.append(self.add_variable_from_reference(
                                 reference_variable=var, name="exp_avg"
@@ -89,14 +88,15 @@ class GaLore(optimizer.Optimizer):
                                 reference_variable=var, name="exp_avg_sq"
                                                     ))
             self.projector.append(None)
+            self.step.append(0)
 
     def update_step(self, gradient, variable, learning_rate):
         lr = tf.cast(learning_rate, variable.dtype)
                 
-        self.step += 1
+        self.step[self._get_variable_index(variable)] += 1
         
-        bias_correction1 = 1 - self.beta1 ** self.step
-        bias_correction2_sq = math.sqrt(1 - self.beta2 ** self.step)
+        bias_correction1 = 1 - self.beta1 ** self.step[self._get_variable_index(variable)]
+        bias_correction2_sq = math.sqrt(1 - self.beta2 ** self.step[self._get_variable_index(variable)])
         
         step_size = lr * bias_correction2_sq / bias_correction1
         
@@ -113,7 +113,7 @@ class GaLore(optimizer.Optimizer):
                     projection_type=self.projection_type,
                 )
 
-            grad = self.projector[self._get_variable_index(variable)].project(gradient, self.step)
+            grad = self.projector[self._get_variable_index(variable)].project(gradient, self.step[self._get_variable_index(variable)])
         
         if self.weight_decouple:
             variable.assign(variable * (1.0 - self.weight_decay * (1.0 if self.fixed_decay else lr)))
@@ -144,7 +144,7 @@ class GaLore(optimizer.Optimizer):
                 "scale": self.scale,
                 "projection_type": self.projection_type,
                 "projector": self.projector,
-                "step": self.iterations.numpy(),
+                "step": [self.iterations.numpy() for _ in range(len(self.step))],
             }
         )
         return config
